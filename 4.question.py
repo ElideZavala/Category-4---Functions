b36e40a2f429db0bff058e3b1d52eff7499c6e98
Elide <zavalavinagreelide@gmail.com>
2025-05-22 22:48:48 -0600

    question 4

--- 4.question.py
+++ 4.question.py
@@ -1,1 +1,5 @@
 # Create a function showEmployee() in such a way that it should accept employee name, and its salary and display both. If the salary is missing in the function call assign default value 9000 for salary
+def employee(name, salary=9000):
+    print(f"El empleado se llama: {name} y tiene un salario de: {salary}")
+
+employee("Elide", 50000)
