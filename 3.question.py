342b69bd6f09721316991c7a14b8a6efbf5a7294
Elide <zavalavinagreelide@gmail.com>
2025-05-22 22:48:38 -0600

    question 3

--- 3.question.py
+++ 3.question.py
@@ -1,1 +1,13 @@
 # Write a function calculation() such that it can accept two variables and calculate the addition and subtraction of them. And also it must return both addition and subtraction in a single return call
+
+def calcualtion(num1, num2):
+    sum = num1 + num2
+    addition = 0
+    if num2 > num1:
+        addition = num2 - num1
+    else:
+        addition = num1 - num2
+
+    return f"La suma es {sum} y la resta es {addition}"
+
+print(calcualtion(21, 23))
