0316f7303001fdfbbaf319a68648716de53e0eeb
Elide <zavalavinagreelide@gmail.com>
2025-05-22 22:26:22 -0600

    question 2

--- 2.question.py
+++ 2.question.py
@@ -1,1 +1,7 @@
 # Write a function such that it can accept a variable length of argument and print all arguments value
+
+def nombres_con_E(*names):
+    for name in names:
+        print(name)
+
+nombres_con_E('Elide', 'Eliceo', 'Eliel', 'Efren', 'Emanuel', 'Edgar', 'Ezequiel')
