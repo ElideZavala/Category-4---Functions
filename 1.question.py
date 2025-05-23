d85033a212768c5948957167068f39654f19634e
Elide <zavalavinagreelide@gmail.com>
2025-05-22 22:09:07 -0600

    question 1

---
+++ 1.question.py
@@ -0,0 +1,6 @@
+# Create a function that can accept two arguments name and age and print their value
+def data_user(name, age):
+    print(f"mi nombre es {name} y tengo {age} de edad")
+
+data_user("Josevo", 19)
+
