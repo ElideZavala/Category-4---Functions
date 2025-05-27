53ce8a60bc305c1896c86e211e4eb765c8652cc6
Elide <zavalavinagreelide@gmail.com>
2025-05-26 22:49:12 -0600

    question 6

--- 6.question.py
+++ 6.question.py
@@ -1,1 +1,21 @@
 # Write a recursive function to calculate the sum of numbers from 0 to 10
+def recursive(num):
+    if (num == 1):
+        return  1
+    else:
+        return num + recursive(num - 1)
+
+
+result = recursive(10)
+print(result)
+
+
+def calculate_sum(num):
+    if num:
+        return num + calculate_sum(num - 1)
+    else:
+        return 0 
+
+
+result2 = calculate_sum(10)
+print(result2)
