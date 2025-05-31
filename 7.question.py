3b72099a7b8d14a6e226a5ea9f8297c1c52548a5
Elide <zavalavinagreelide@gmail.com>
2025-05-30 18:09:36 -0600

    question 7

--- 7.question.py
+++ 7.question.py
@@ -1,1 +1,9 @@
 # Assign a different name to a function and call it with the new name
+def first_name(name):
+    return f"Hola Hombre, {name}"
+
+second_name = first_name
+
+result = second_name('Marcos')
+
+print(result)
