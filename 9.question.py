76f23076e0174fef1d8d179f9fc43e8b95ca3307
Elide <zavalavinagreelide@gmail.com>
2025-05-30 18:09:53 -0600

    question 9

--- 9.question.py
+++ 9.question.py
@@ -1,3 +1,15 @@
 # Return the largest item from the given list
 
 list = [4, 6, 8, 24, 12, 2]
+
+def more_big(numbers):
+    number_more_big = numbers[0]
+    
+    for number in numbers:
+            if number > number_more_big:
+                number_more_big = number
+    
+    return number_more_big
+
+print(more_big(list))
+# more_big(list)
