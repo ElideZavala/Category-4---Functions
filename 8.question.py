c708d5c8a20d3652cb9832b5cc8d1499b8c243cf
Elide <zavalavinagreelide@gmail.com>
2025-05-30 18:09:47 -0600

    question 8

--- 8.question.py
+++ 8.question.py
@@ -1,1 +1,15 @@
 # Generate a Python list of all the even numbers between 4 to 30
+
+def numbers_pars( start, end):
+    list = []
+    for i in range(start, end):
+        if (i > 1 & i % 2 == 0) :
+            list.append(i)
+    return list
+
+pars = numbers_pars(4, 30)
+print(pars)
+
+print("\n****************************************")
+
+print(list(range(4, 30, 2)))
